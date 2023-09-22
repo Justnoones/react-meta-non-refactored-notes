b1cc9ada1a8a037c7abea216a0c58d4d6a0520b6
Justnoones <yanw97841@gmail.com>
2023-09-22 20:39:17 +0630

    children and clone-element

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import './Row.css';
 
 export default function Row ({children}) {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
   return (
-    <div className='manipulate-parent'>
-        {React.Children.map(children, (child, index) => (
-            <div className='manipulated-child'>{child}</div>
-        ))}
+    <div className="List">
+      {React.Children.map(children, (child, index) =>
+        React.cloneElement(child, {
+          isHighlighted: index === selectedIndex 
+        })
+      )}
+      <hr />
+      <button onClick={() => {
+        setSelectedIndex(i =>
+          (i + 1) % React.Children.count(children)
+        );
+      }}>
+        Next
+      </button>
     </div>
+  );
+}
+
+export function Column ({ title, isHighlighted }) {
+  return (
+    <h1 className={[
+      'Row',
+      isHighlighted ? 'RowHighlighted' : ''
+    ].join(' ')}>
+      {title}
+    </h1>
   )
 }
